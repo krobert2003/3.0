79aba7feaa5a8fded7387065882d58b876cbd837
cisco <cisco@B101-03>
2022-09-21 13:17:41 +0200

    Add project files.

---
+++ Githubos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Githubos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adj meg egy számot");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adj meg egy számot");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a>b)
+            {
+                Console.WriteLine("Nagyobb az első szám");
+            }
+            else
+            {
+                Console.WriteLine("Nagyobb a második szám");
+            }
+            Console.ReadKey();
+        }
+    }
+}
